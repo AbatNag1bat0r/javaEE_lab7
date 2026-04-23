a75a9d17adb43797357d5fa292b1e8744926aa7b
Abat <abat.nagibat0r@gmail.com>
2026-04-23 23:48:30 +0500

    Добавлен UNIT и WEB тесты; Добавлен Log с сохранением

--- src/main/java/com/example/lab7/Lab7Application.java
+++ src/main/java/com/example/lab7/Lab7Application.java
@@ -2,12 +2,24 @@ package com.example.lab7;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.scheduling.annotation.EnableAsync;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
 
+@Slf4j
 @SpringBootApplication
 @EnableAsync
 public class Lab7Application {
     public static void main(String[] args) {
         SpringApplication.run(Lab7Application.class, args);
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void actionAfterStartup() {
+        log.info("=======================================");
+        log.info("Лабораторная работа №7 успешно запущена!");
+        log.info("Проект готов к работе с MongoDB, JWT, UNIT-тест, Web-тест и Logging .");
+        log.info("=======================================");
     }
 }
